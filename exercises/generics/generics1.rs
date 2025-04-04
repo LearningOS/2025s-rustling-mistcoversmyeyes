9b50e16db579b5f0b2b90ffecbf3710cfe503318
Yuming <mingjiangyu1@qq.com>
2025-04-05 00:22:06 +0800

    简单了解了一下泛型

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -1,14 +1,12 @@
 // generics1.rs
 //
-// This shopping list program isn't compiling! Use your knowledge of generics to
-// fix it.
+// 这个购物清单程序无法编译！使用你对泛型的了解来修复它。
 //
-// Execute `rustlings hint generics1` or use the `hint` watch subcommand for a
-// hint.
+// 执行 `rustlings hint generics1` 或使用 `hint` watch 子命令获取提示。
 
 // I AM NOT DONE
 
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
