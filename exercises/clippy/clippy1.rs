2e672f6ec213889d3d435e1fce73a1e5224696a0
Yuming <mingjiangyu1@qq.com>
2025-04-12 00:14:19 +0800

    clippy实际上是一个帮助你改进代码的lints 集合

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -1,20 +1,19 @@
 // clippy1.rs
 //
-// The Clippy tool is a collection of lints to analyze your code so you can
-// catch common mistakes and improve your Rust code.
-//
-// For these exercises the code will fail to compile when there are clippy
-// warnings check clippy's suggestions from the output to solve the exercise.
+// Clippy 工具是一个 lint 集合，用于分析您的代码，以便您可以
+// 捕获常见错误并改进您的 Rust 代码。
+// 
+// 对于这些练习，当有 clippy 警告时，代码将无法编译
+// 请查看输出中 clippy 的建议来解决练习。
 //
 // Execute `rustlings hint clippy1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::f32;
 
 fn main() {
-    let pi = 3.14f32;
+    let pi = f32::consts::PI;
     let radius = 5.00f32;
 
     let area = pi * f32::powi(radius, 2);
